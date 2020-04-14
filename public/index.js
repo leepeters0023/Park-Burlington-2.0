dc49a4150632c248b9a7f61d0b7946c39ee2a04a
Lee Peters <lpeters0023@gmail.com>
2020-04-14 11:19:15 -0400

    who knows

--- public/index.js
+++ public/index.js
@@ -1,5 +1,3 @@
-//Firebase Configuration
-
 const config = {
   apiKey: "AIzaSyCJ_q627N1dryTYbcSjE4d-4jfsJJg5VcY",
   authDomain: "park-burlington.firebaseapp.com",
@@ -13,58 +11,37 @@ const config = {
 
 firebase.initializeApp(config)
 
-var database = firebase.database()
-var ref = database.ref()
+let database = firebase.database()
+let ref = database.ref()
 
-ref.on('value', gotData, errData)
+async function makeQuery() {
+    let myVar = await ref.once('value')
+      .then(function(dataSnapshot) {
+        let info = dataSnapshot.val()
+        let keys = Object.keys(info)
+        for (var i = 0; i < keys.length; i++) {
+          let k = keys[i]
+          let name = info[k].name
+          let coords = info[k].coordinates
+          let descrip = info[k].description
+        }
+        return dataSnapshot.val();
+      });
+      return myVar;
+  }
 
-
-//Accesses the information from firebase
-//and lists it in key order by name, descrip and coords
-function gotData(data) {
-    // console.log(data.val())
-    let info = data.val()
-    let keys = Object.keys(info)
-    //console.log(keys)
-    for (var i = 0; i < keys.length; i++) {
-        var k = keys[i]
-        var name = info[k].name
-        var coords = info[k].coordinates
-        var descrip = info[k].description
-    //console.log(name, coords, descrip)
-            
-    // Below creates list of everything from database and print in html:
-        // var li = document.createElement('li')
-        // li.innerHTML = (k + ') ' + name + ': \n' +
-        //     descrip + '\n ' + '\n'+coords)
-        // document.body.appendChild(li)
-
-    }
-}
 //Error Message if you cant get in-------------------------
 function errData(data) {
     console.log('Error!')
     console.log(err)
 }
-
-
-
 //------------------------------------------------------------
 
 async function initMap() {
+  
   //Fetch geojson
-  let togglePolyLayer = document.getElementById('togglePolygons')
-  let polyLayerOn = 'off'
-  let toggleLineStringLayer = document.getElementById('toggleLineString')
-  let lineStringLayerOn = 'off'
-
-  const linestringData = await fetch("./parkingByType-geoJSONfiles/BurlingtonParkingLineString.geojson")
-      .then(res => res.json())
-      .then(res => res)
-  const polygonData = await fetch("./parkingByType-geoJSONfiles/BurlingtonParkingPolygon.geojson")
-      .then(res => res.json())
-      .then(res => res)
-      
+  const mapData = await fetch("./BurlingtonParkingMap.geojson").then( res => res.json()).then( res => res)
+  console.log(mapData)
 
   //Define lat lng location of the center of downtown Burlington
   const burlingtonCenter = {lat: 44.478081, lng: -73.215}
@@ -72,6 +49,8 @@ async function initMap() {
   //Define a 1.5 mile (2414.02) circle around downtown Burlington
   const circle = new google.maps.Circle(
     {center: burlingtonCenter, radius: 2414.02});
+let myInfo = await makeQuery()
+console.log({myInfo});
 
   //Define max lat lng view limits of the map
   const viewLimit = {
@@ -79,10 +58,9 @@ async function initMap() {
     south: 44.424518,
     west: -73.269027,
     east: -73.151240,
- }
-
+  }
   //Initialize map with some controls disabled
-  const map = new google.maps.Map(document.getElementById('map'), {
+  let map = new google.maps.Map(document.getElementById('map'), {
     center: burlingtonCenter,
     zoom: 15.3,
     fullscreenControl: false,
@@ -148,108 +126,68 @@ async function initMap() {
     
   });
     
-  let polyLayer = new google.maps.Data();
-  let lineStringLayer = new google.maps.Data();
+  // Import parking map geojson file
 
-  // Create layers
-  lineStringLayer.addGeoJson(linestringData);
-  polyLayer.addGeoJson(polygonData);
+  // add polygons to map from FireBase
+  let state = {
+    "name": [],
+    "coords" : []
+  }
 
-  // set layers on map
-  lineStringLayer.setMap(map);
-  polyLayer.setMap(map);
-
-  // Set initial styles for data layers
-  polyLayer.setStyle(function (feature) {
-      let fillC = feature.getProperty('fill')
-      let fillO = feature.getProperty('fill-opacity')
-      let strokeC = feature.getProperty('stroke')
-      let strokeO = feature.getProperty('stroke-opacity')
-      let strokeW = feature.getProperty('stroke-width')
-
-      return {
-          fillColor: fillC,
-          fillOpacity: fillO,
-          strokeColor: strokeC,
-          strokeOpacity: strokeO,
-          strokeWeight: strokeW,
-      }   
+myInfo.forEach((item) => {
+  let path = item.coordinates.split('0,')
+  let newPath = path.map((item) => {
+    let coordPair = item.split(',')
+    return {lat: Number(coordPair[1]), lng: Number(coordPair[0])}
+    
   })
-
-  lineStringLayer.setStyle(function (feature) {
-      let fillC = feature.getProperty('fill');
-      let fillO = feature.getProperty('fill-opacity')
-      let strokeC = feature.getProperty('stroke')
-      let strokeO = feature.getProperty('stroke-opacity')
-      let strokeW = feature.getProperty('stroke-width')
-
-      return {
-          fillColor: fillC,
-          fillOpacity: fillO,
-          strokeColor: strokeC,
-          strokeOpacity: strokeO,
-          strokeWeight: strokeW,
-      };
+  
+  let polygonLayer = new google.maps.Polygon({
+    paths: newPath,
+    strokeColor: '#FF0000', // here we'll refer back to the object so item.whatever
+    strokeOpacity: 0.8,
+    strokeWeight: 2,
+    fillColor: '#FF0000',
+    fillOpacity: 0.35
   });
+  polygonLayer.setMap(map);
+})
 
+  let keys = Object.keys(myInfo)
+        for (var i = 0; i < keys.length; i++) {
+          let k = keys[i]
+          state.name.push(myInfo[k].name)
+          state.coords.push(myInfo[k].coordinates)
+          let descrip = myInfo[k].description
+        }
 
-// toggle fuctions turn data layers on and off  
-  togglePolyLayer.addEventListener('click', function () {
-      //console.log(polyLayerOn)
-      if (polyLayerOn === 'off') {
-          polyLayer.setStyle({ visible: false })
-          polyLayerOn = 'on'
-      } else if (polyLayerOn === 'on') {
-          polyLayer.setStyle(function (feature) {
-              let fillC = feature.getProperty('fill')
-              let fillO = feature.getProperty('fill-opacity')
-              let strokeC = feature.getProperty('stroke')
-              let strokeO = feature.getProperty('stroke-opacity')
-              let strokeW = feature.getProperty('stroke-width')
-      
-              return {
-                  fillColor: fillC,
-                  fillOpacity: fillO,
-                  strokeColor: strokeC,
-                  strokeOpacity: strokeO,
-                  strokeWeight: strokeW,
-              }
-          })
-          polyLayerOn = 'off'
-      }
+ 
+
+  // Import line/polygon styles from mapData objects
+  map.data.setStyle(function(feature) {
+    let fillC = feature.getProperty('fill');
+    let fillO = feature.getProperty('fill-opacity')
+    let strokeC = feature.getProperty('stroke')
+    let strokeO = feature.getProperty('stroke-opacity')
+    let strokeW = feature.getProperty('stroke-width')
+    let iconImg = feature.getProperty('icon')
+    
+    return {
+      fillColor: fillC,
+      fillOpacity:  fillO,
+      strokeColor: strokeC,
+      strokeOpacity: strokeO,
+      strokeWeight: 3,
+      icon: iconImg,
+    };
   });
-
-  toggleLineStringLayer.addEventListener('click', function () {
-      //.log(lineStringLayerOn)
-      if (lineStringLayerOn === 'off') {
-          lineStringLayer.setStyle({ visible: false })
-          lineStringLayerOn = 'on'
-      } else if (lineStringLayerOn === 'on') {
-          lineStringLayer.setStyle(function (feature) {
-              let fillC = feature.getProperty('fill')
-              let fillO = feature.getProperty('fill-opacity')
-              let strokeC = feature.getProperty('stroke')
-              let strokeO = feature.getProperty('stroke-opacity')
-              let strokeW = feature.getProperty('stroke-width')
-      
-              return {
-                  fillColor: fillC,
-                  fillOpacity: fillO,
-                  strokeColor: strokeC,
-                  strokeOpacity: strokeO,
-                  strokeWeight: strokeW,
-              }
-          })
-          lineStringLayerOn = 'off'
-      }
-  });
-
 
   //Create info window cards and add click listeners to each parking asset
-  var infowindow = new google.maps.InfoWindow({
+  let infowindow = new google.maps.InfoWindow({
     content: ""
   });
-  lineStringLayer.addListener('click', function(event) {      
+  map.data.addListener('click', function(event) {
+    console.log(event)
     let name = event.feature.getProperty('name');
     let description = event.feature.getProperty('description');
     let html = '<strong>'+ name + '</strong>' + '<br><br>' + description;
@@ -262,12 +200,12 @@ async function initMap() {
   });
 
   //Get searchbox element and fix it to top left of screen
-  var card = document.getElementById('pac-card');
-  var input = document.getElementById('pac-input');
+  let card = document.getElementById('pac-card');
+  let input = document.getElementById('pac-input');
   map.controls[google.maps.ControlPosition.TOP_LEFT].push(card);
 
   //Initialize autocomplete function in the searchbar
-  var autocomplete = new google.maps.places.Autocomplete(input);
+  let autocomplete = new google.maps.places.Autocomplete(input);
   
  // Limit autocomplete results to within a 2 mile (3218.688 meter) circle of downtown Burlington.
   autocomplete.setBounds(circle.getBounds());
@@ -277,10 +215,10 @@ async function initMap() {
   autocomplete.setFields(
       ['address_components', 'geometry', 'icon', 'name']);
 
-  var addressinfowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
+  let addressinfowindow = new google.maps.InfoWindow();
+  let infowindowContent = document.getElementById('infowindow-content');
   addressinfowindow.setContent(infowindowContent);
-  var marker = new google.maps.Marker({
+  let marker = new google.maps.Marker({
     map: map,
     anchorPoint: new google.maps.Point(0, -29)
   });
@@ -288,7 +226,7 @@ async function initMap() {
   autocomplete.addListener('place_changed', function() {
     infowindow.close();
     marker.setVisible(false);
-    var place = autocomplete.getPlace();
+    let place = autocomplete.getPlace();
     if (!place.geometry) {
       // User entered the name of a Place that was not suggested and
       // pressed the Enter key, or the Place Details request failed.
@@ -306,7 +244,7 @@ async function initMap() {
     marker.setPosition(place.geometry.location);
     marker.setVisible(true);
 
-    var address = '';
+    let address = '';
     if (place.address_components) {
       address = [
         (place.address_components[0] && place.address_components[0].short_name || ''),
