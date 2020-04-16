d6f14d450444162ff434613f8f195e7880d9fdaa
Lee Peters <lpeters0023@gmail.com>
2020-04-16 16:04:21 -0400

    proxy server functional

---
+++ Public/api.js
@@ -0,0 +1,16 @@
+let script = document.createElement('script')
+
+
+fetch('/apikey')
+  .then((response) => {
+    return response.text()
+  }).then((apiKey) => {
+    let googleReq = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&libraries=places&callback=initMap`;
+    script.setAttribute('src', googleReq)
+    script.setAttribute('async', true)
+    script.setAttribute('defer', true)
+    document.body.appendChild(script)
+  })
+
+
+
