35b773a93a75f4a0f2daa53f1ca053b04c6a9b25
Lee Peters <lpeters0023@gmail.com>
2020-04-19 10:44:02 -0400

    add that P

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+
 const express = require('express'); // this is a safer way rather than import because import is newer and not as widely adopted 
 const path = require('path'); 
 const app = express();
 const port = process.env.PORT || 5000; 
 require('dotenv').config()
 
-app.use(express.static(path.join(__dirname, "/public"))); // sets up a static directory from which our files are served and prevents people seeing sensitive code 
+app.use(express.static(path.join(__dirname, "/Public"))); // sets up a static directory from which our files are served and prevents people seeing sensitive code 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/Public/index.html"));
 }) // * means 'any path' - __dirname joins our current directory to the following string 
