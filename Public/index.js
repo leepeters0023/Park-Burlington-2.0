d3212ae42f3b21de25c0cbfebd1d0ecfef173652
Lee Peters <lpeters0023@gmail.com>
2020-04-22 14:51:18 -0400

    laurens key hidden

--- Public/index.js
+++ Public/index.js
@@ -13,7 +13,7 @@ window.addEventListener('resize', () => {
 
 //***********Firebase Configuration ****************************************************************
 const config = {
-  apiKey: "AIzaSyCJ_q627N1dryTYbcSjE4d-4jfsJJg5VcY",
+  apiKey: 'process.env.FIREBASE_KEY',
   authDomain: "park-burlington.firebaseapp.com",
   databaseURL: "https://park-burlington.firebaseio.com",
   projectId: "park-burlington",
@@ -233,23 +233,6 @@ async function initMap() {
       icon: null,
       optimized: false,
     });
-    
-    priceIcon.addListener('click', function (event) {
-      if (activeWindow != null) {
-        activeWindow.close()
-      }
-      let html = '<strong>' + name + '</strong>' + '<br><br>' + description;
-      infowindow.setContent(html)
-
-      infowindow.setPosition(event.latLng);
-      infowindow.setOptions({
-        pixelOffset: new google.maps.Size(0, 0)
-      }); // move the infowindow up slightly to the top of the marker icon
-      infowindow.open(map);
-      { passive: true }
-      activeWindow = infowindow;
-    });
-
 
     if (rate != "") {
       let dynamicFontSize = (24 - (rate.length * 1.2)).toString() + 'px'
